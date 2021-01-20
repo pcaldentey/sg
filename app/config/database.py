3c2ede30b50ba60a7dd8f8364c45234668b25b28
Pablo Caldentey <pablo@caldentey.org>
2021-01-20 05:22:54 +0100

    Database models generated using reflection, and db connection

--- app/config/database.py
+++ app/config/database.py
@@ -1,1 +1,20 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app/bin/database/chinook.db'
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:////app/bin/database/chinook.db'
+Base = automap_base()
+
+# engine
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# mapped classes are now created with names by default
+# matching that of the table name.
+Album = Base.classes.albums
+Track = Base.classes.tracks
+Artist = Base.classes.artists
+
+session = Session(engine)
